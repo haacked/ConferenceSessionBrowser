151b0e30b8a00c7cb856532fca4aa347ea0020a1
Haacked <haacked@gmail.com>
2011-08-28 16:16:18 -0700

    Adding account controller

---
+++ ConferenceSessionBrowser.Web/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ConferenceSessionBrowser.Controllers {
+    public class AccountController : Controller {
+        public ActionResult LogOn() {
+            return View();
+        }
+
+        public ActionResult Register() {
+            return View();
+        }
+    }
+}
